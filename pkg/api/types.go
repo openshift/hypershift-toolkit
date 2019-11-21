4e7da0210a7a0ef06a838b9b09cf13feef5dd3e4
Bruce Cong <BRUCE.CONG@ibm.com@bruces-mbp.austin.ibm.com>
2019-11-21 11:13:34 -0600

    Added way to add named certificates to kube-apiserver config file
    made formatting a bit neater
    
    forgot to add quotes
    
    changed to add any number of named certs
    
    using array of structs to make things a bit cleaner
    
    changed the example to showcase how to write it up
    
    made things a bit neater
    
    accidentally put .crt instead of .key
    
    seems like I need to change this for the config to actually change
    
    modified script to use full path instead of relative path
    
    checking changes made to the config file
    
    removed print statements and modified bindata file
    
    changed namedcerts struct to singular case
    
    updated example to contain full path to certs

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -1,21 +1,27 @@
 package api
 
 type ClusterParams struct {
-	Namespace               string `json:"namespace"`
-	ExternalAPIDNSName      string `json:"externalAPIDNSName"`
-	ExternalAPIPort         uint   `json:"externalAPIPort"`
-	ExternalAPIIPAddress    string `json:"externalAPIAddress"`
-	ExternalOpenVPNDNSName  string `json:"externalVPNDNSName"`
-	ExternalOpenVPNPort     uint   `json:"externalVPNPort"`
-	ServiceCIDR             string `json:"serviceCIDR"`
-	PodCIDR                 string `json:"podCIDR"`
-	ReleaseImage            string `json:"releaseImage"`
-	APINodePort             uint   `json:"apiNodePort"`
-	IngressSubdomain        string `json:"ingressSubdomain"`
-	OpenShiftAPIClusterIP   string `json:"openshiftAPIClusterIP"`
-	ImageRegistryHTTPSecret string `json:"imageRegistryHTTPSecret"`
-	RouterNodePortHTTP      string `json:"routerNodePortHTTP"`
-	RouterNodePortHTTPS     string `json:"routerNodePortHTTPS"`
-	OpenVPNNodePort         string `json:"openVPNNodePort"`
-	BaseDomain              string `json:"baseDomain"`
+	Namespace               string       `json:"namespace"`
+	ExternalAPIDNSName      string       `json:"externalAPIDNSName"`
+	ExternalAPIPort         uint         `json:"externalAPIPort"`
+	ExternalAPIIPAddress    string       `json:"externalAPIAddress"`
+	ExternalOpenVPNDNSName  string       `json:"externalVPNDNSName"`
+	ExternalOpenVPNPort     uint         `json:"externalVPNPort"`
+	ServiceCIDR             string       `json:"serviceCIDR"`
+	NamedCerts              []NamedCert  `json:"namedCerts,omitempty"`
+	PodCIDR                 string       `json:"podCIDR"`
+	ReleaseImage            string       `json:"releaseImage"`
+	APINodePort             uint         `json:"apiNodePort"`
+	IngressSubdomain        string       `json:"ingressSubdomain"`
+	OpenShiftAPIClusterIP   string       `json:"openshiftAPIClusterIP"`
+	ImageRegistryHTTPSecret string       `json:"imageRegistryHTTPSecret"`
+	RouterNodePortHTTP      string       `json:"routerNodePortHTTP"`
+	RouterNodePortHTTPS     string       `json:"routerNodePortHTTPS"`
+	OpenVPNNodePort         string       `json:"openVPNNodePort"`
+	BaseDomain              string       `json:"baseDomain"`
+}
+
+type NamedCert struct {
+	NamedCertPrefix  string `json:"namedCertPrefix"`
+	NamedCertDomain  string `json:"namedCertDomain"`
 }
