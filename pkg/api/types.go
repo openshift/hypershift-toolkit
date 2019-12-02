84a454f95eea9fad633ba61a9751f2c038865f2d
KodieGlosserIBM <39170759+KodieGlosserIBM@users.noreply.github.com>
2019-12-02 09:09:02 -0600

    integrate with IBMCloud internal repos (#10)
    * integrating with downstream IBM cloud repos
    
    * update
    
    * ghosts
    
    * image additions
    
    * image additions
    
    * logic
    
    * asset changes
    
    * remove idea
    
    * add tolerations and affinity rules
    
    * fix naming

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -1,27 +1,31 @@
 package api
 
 type ClusterParams struct {
-	Namespace               string       `json:"namespace"`
-	ExternalAPIDNSName      string       `json:"externalAPIDNSName"`
-	ExternalAPIPort         uint         `json:"externalAPIPort"`
-	ExternalAPIIPAddress    string       `json:"externalAPIAddress"`
-	ExternalOpenVPNDNSName  string       `json:"externalVPNDNSName"`
-	ExternalOpenVPNPort     uint         `json:"externalVPNPort"`
-	ServiceCIDR             string       `json:"serviceCIDR"`
-	NamedCerts              []NamedCert  `json:"namedCerts,omitempty"`
-	PodCIDR                 string       `json:"podCIDR"`
-	ReleaseImage            string       `json:"releaseImage"`
-	APINodePort             uint         `json:"apiNodePort"`
-	IngressSubdomain        string       `json:"ingressSubdomain"`
-	OpenShiftAPIClusterIP   string       `json:"openshiftAPIClusterIP"`
-	ImageRegistryHTTPSecret string       `json:"imageRegistryHTTPSecret"`
-	RouterNodePortHTTP      string       `json:"routerNodePortHTTP"`
-	RouterNodePortHTTPS     string       `json:"routerNodePortHTTPS"`
-	OpenVPNNodePort         string       `json:"openVPNNodePort"`
-	BaseDomain              string       `json:"baseDomain"`
+	Namespace               string      `json:"namespace"`
+	ExternalAPIDNSName      string      `json:"externalAPIDNSName"`
+	ExternalAPIPort         uint        `json:"externalAPIPort"`
+	ExternalAPIIPAddress    string      `json:"externalAPIAddress"`
+	ExternalOpenVPNDNSName  string      `json:"externalVPNDNSName"`
+	ExternalOpenVPNPort     uint        `json:"externalVPNPort"`
+	ServiceCIDR             string      `json:"serviceCIDR"`
+	NamedCerts              []NamedCert `json:"namedCerts,omitempty"`
+	PodCIDR                 string      `json:"podCIDR"`
+	ReleaseImage            string      `json:"releaseImage"`
+	APINodePort             uint        `json:"apiNodePort"`
+	IngressSubdomain        string      `json:"ingressSubdomain"`
+	OpenShiftAPIClusterIP   string      `json:"openshiftAPIClusterIP"`
+	ImageRegistryHTTPSecret string      `json:"imageRegistryHTTPSecret"`
+	RouterNodePortHTTP      string      `json:"routerNodePortHTTP"`
+	RouterNodePortHTTPS     string      `json:"routerNodePortHTTPS"`
+	OpenVPNNodePort         string      `json:"openVPNNodePort"`
+	BaseDomain              string      `json:"baseDomain"`
+	NetworkType             string      `json:"networkType"`
+	Replicas                string      `json:"replicas"`
+	EtcdClientName          string      `json:"etcdClientName"`
+	OriginReleasePrefix     string      `json:"originReleasePrefix"`
 }
 
 type NamedCert struct {
-	NamedCertPrefix  string `json:"namedCertPrefix"`
-	NamedCertDomain  string `json:"namedCertDomain"`
+	NamedCertPrefix string `json:"namedCertPrefix"`
+	NamedCertDomain string `json:"namedCertDomain"`
 }
