392aa1988b32bb11d53f8e7204ced1ba01d4df8c
KodieGlosserIBM <39170759+KodieGlosserIBM@users.noreply.github.com>
2019-12-11 21:35:26 -0600

    Update for master (#36)
    * Properly configure the APIServices for openshift (#13)
    
    * switch to old cvo operator
    
    * update release
    
    * update
    
    * update data
    
    * update with ca bundle for openshift apiservice
    
    * Ca operator (#14)
    
    * add ca-operator
    
    * cli image
    
    * Cloud provider (#18) (#19)
    
    * Properly configure the APIServices for openshift (#13)
    
    * switch to old cvo operator
    
    * update release
    
    * update
    
    * update data
    
    * update with ca bundle for openshift apiservice
    
    * Ca operator (#14)
    
    * add ca-operator
    
    * cli image
    
    * apiserver config additions
    
    * make cloud provider a variable
    
    * new line
    
    (cherry picked from commit 55b9b4731be40a9abba8d9b84efe59ac07201824)
    
    * needed label for openvpn operator
    
    * needed label for openvpn operator (#20)
    
    * update assets with namespace (#21)
    
    * fix ca-operator and variables
    
    * wrong spot
    
    * wrong spot
    
    * use json patch instead
    
    * formatt
    
    * cleanup
    
    * fix ca-operator and variables (#22)
    
    * needed label for openvpn operator
    
    * fix ca-operator and variables
    
    * wrong spot
    
    * wrong spot
    
    * use json patch instead
    
    * formatt
    
    * cleanup
    
    * rename
    
    * Rename file to not conflict with CO CRD (#23)
    
    * needed label for openvpn operator
    
    * fix ca-operator and variables
    
    * wrong spot
    
    * wrong spot
    
    * use json patch instead
    
    * formatt
    
    * cleanup
    
    * rename
    
    * Fix oauth server and properly allow toggle between external and internal oauth server (#24)
    
    * update oauth server
    
    * fix oauth server
    
    * also apply kubelet serving cert appropriately
    
    * add configuration to init container (#25)
    
    * Add owners (#27)
    
    * Fix owners (#29)
    
    * Add make verify (#31)
    
    * add internal api port  (#32)
    
    * update
    
    * update asset data
    
    * Fix port specification logic (#33)
    
    * update
    
    * update asset data
    
    * update port specification
    
    * add ability to specify advertised port
    
    * update port specification
    
    * update port specification
    
    * Remove asset versioning (#34)
    
    * needed label for openvpn operator
    
    * fix ca-operator and variables
    
    * wrong spot
    
    * wrong spot
    
    * use json patch instead
    
    * formatt
    
    * cleanup
    
    * rename
    
    * remove asset versioning. Versioning will be done at the branch level now instead
    
    * remove more v420 assets
    
    * use other cvo

--- hack/update-generated-bindata.sh
+++ hack/update-generated-bindata.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
-OUTPUTFILE="${OUTPUTFILE:-${REPO_DIR}/pkg/assets/v420_assets/bindata.go}"
+OUTPUTFILE="${OUTPUTFILE:-${REPO_DIR}/pkg/assets/bindata.go}"
 
 TMP_GOPATH="$(mktemp -d)"
 
@@ -13,6 +13,6 @@ pushd ${REPO_DIR} &> /dev/null
 GO111MODULE=off GOPATH="${TMP_GOPATH}" go install "./vendor/github.com/jteeuwen/go-bindata/..."
 popd &> /dev/null
 
-"${TMP_GOPATH}/bin/go-bindata" -nocompress -nometadata -prefix "${REPO_DIR}/assets/v4.2.0" -pkg v420_assets -o "${OUTPUTFILE}" "${REPO_DIR}/assets/v4.2.0/..."
+"${TMP_GOPATH}/bin/go-bindata" -nocompress -nometadata -prefix "${REPO_DIR}/assets/" -pkg assets -o "${OUTPUTFILE}" "${REPO_DIR}/assets/..."
 
 gofmt -s -w "${OUTPUTFILE}"
