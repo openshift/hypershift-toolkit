d2c31a01d9762a0953b5f31f0824a6b4c11813b1
Kodie Glosser <krglosse@us.ibm.com>
2020-01-30 18:04:52 -0600

    APIServer Audit log support
    APIServer Audit log support
    
    optional everything
    
    Update config.yaml
    
    Update types.go
    
    Update cluster.yaml.example
    
    variable policy
    
    format
    
    change name
    
    make it a bool
    
    bindata
    
    Update kube-apiserver-deployment.yaml
    
    Update kube-apiserver-deployment.yaml
    
    Update config.yaml
    
    bindata
    
    up
    
    up

--- pkg/assets/bindata.go
+++ pkg/assets/bindata.go
@@ -1531,6 +1531,11 @@ auditConfig:
   logFormat: json
   maximumFileSizeMegabytes: 100
   maximumRetainedFiles: 10
+{{ if .APIServerAuditEnabled }}
+  policyFile: /etc/kubernetes/audit/policy.yaml
+  webHookKubeConfig: /etc/kubernetes/audit/webhook-kubeconfig
+  webHookMode: batch
+{{ else }}
   policyConfiguration:
     apiVersion: audit.k8s.io/v1beta1
     kind: Policy
@@ -1560,6 +1565,7 @@ auditConfig:
     - level: Metadata
       omitStages:
       - RequestReceived
+{{ end }}
 authConfig:
   oauthMetadataFile: "/etc/kubernetes/oauth/oauthMetadata.json"
   requestHeader:
@@ -1777,6 +1783,10 @@ spec:
           name: oauth
         - mountPath: /var/log/kube-apiserver/
           name: logs
+{{ if .APIServerAuditEnabled }}
+        - name: apiserver-cm
+          mountPath: /etc/kubernetes/audit/
+{{ end }}
 {{ if includeVPN }}
       - name: openvpn-client
         image: quay.io/sjenning/poc:openvpn
@@ -1809,6 +1819,11 @@ spec:
       - configMap:
           name: kube-apiserver-oauth-metadata
         name: oauth
+{{ if .APIServerAuditEnabled }}
+      - name: apiserver-cm
+        configMap:
+          name: apiserver-audit-cm
+{{ end }}
 {{ if includeVPN }}
       - configMap:
           name: kube-apiserver-vpnclient-config
