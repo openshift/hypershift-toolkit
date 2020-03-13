c000e71e0f24045433f77c1bd9825880bc0c3fae
Cesar Wong <cewong@redhat.com>
2020-03-13 13:02:41 -0400

    control plane controller: remove management cluster informers

--- pkg/controllers/kubelet_serving_ca/syncer.go
+++ pkg/controllers/kubelet_serving_ca/syncer.go
@@ -1,7 +1,6 @@
 package kubelet_serving_ca
 
 import (
-	"context"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -11,36 +10,26 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubeclient "k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // syncInterval is the amount of time to use between checks
-var syncInterval = 5 * time.Minute
+var syncInterval = 20 * time.Minute
+
+// controlPlaneOperatorConfig is the name of the source configmap on the management cluster
+const controlPlaneOperatorConfig = "control-plane-operator"
 
 type KubeletServingCASyncer struct {
-	client.Client
-	Namespace    string
 	TargetClient kubeclient.Interface
 	Log          logr.Logger
+	InitialCA    string
 }
 
 func (s *KubeletServingCASyncer) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	ctx := context.Background()
-	if req.Namespace != s.Namespace {
-		return result(nil)
-	}
-	if req.Name != ControlPlaneOperatorConfig {
-		return result(nil)
-	}
-	cpConfig := &corev1.ConfigMap{}
-	if err := s.Get(ctx, req.NamespacedName, cpConfig); err != nil {
-		return ctrl.Result{}, err
-	}
 	targetConfigMap, err := s.TargetClient.CoreV1().ConfigMaps("openshift-config-managed").Get("kubelet-serving-ca", metav1.GetOptions{})
 	if err != nil && !errors.IsNotFound(err) {
 		return result(err)
 	}
-	expectedConfigMap := s.expectedConfigMap(cpConfig)
+	expectedConfigMap := s.expectedConfigMap()
 	if err != nil {
 		s.Log.Info("target configmap not found, creating it")
 		_, err = s.TargetClient.CoreV1().ConfigMaps("openshift-config-managed").Create(expectedConfigMap)
@@ -61,12 +50,12 @@ func result(err error) (ctrl.Result, error) {
 	return ctrl.Result{RequeueAfter: syncInterval}, nil
 }
 
-func (s *KubeletServingCASyncer) expectedConfigMap(source *corev1.ConfigMap) *corev1.ConfigMap {
+func (s *KubeletServingCASyncer) expectedConfigMap() *corev1.ConfigMap {
 	cm := &corev1.ConfigMap{}
 	cm.Name = "kubelet-serving-ca"
 	cm.Namespace = "openshift-config-managed"
 	cm.Data = map[string]string{
-		"ca-bundle.crt": source.Data["initial-ca.crt"],
+		"ca-bundle.crt": s.InitialCA,
 	}
 	return cm
 }
