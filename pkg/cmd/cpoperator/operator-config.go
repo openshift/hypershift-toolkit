a2da8ed42bff1e2e07ae78cf6ed1b5535ec1ffa9
Cesar Wong <cewong@redhat.com>
2020-03-13 13:02:41 -0400

    control plane controller: remove management cluster informers

--- pkg/cmd/cpoperator/operator-config.go
+++ pkg/cmd/cpoperator/operator-config.go
@@ -7,11 +7,18 @@ import (
 	"github.com/go-logr/logr"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/informers"
 	kubeclient "k8s.io/client-go/kubernetes"
 	kubescheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	configclient "github.com/openshift/client-go/config/clientset/versioned"
+	configinformers "github.com/openshift/client-go/config/informers/externalversions"
+
+	common "github.com/openshift/hypershift-toolkit/pkg/controllers"
 )
 
 type ControllerSetupFunc func(*ControlPlaneOperatorConfig) error
@@ -32,15 +39,17 @@ type ControlPlaneOperatorConfig struct {
 	config           *rest.Config
 	targetConfig     *rest.Config
 	targetKubeClient kubeclient.Interface
+	kubeClient       kubeclient.Interface
 	logger           logr.Logger
 	scheme           *runtime.Scheme
 
-	versions         map[string]string
-	targetKubeconfig string
-	namespace        string
-	initialCA        []byte
-	controllers      []string
-	controllerFuncs  map[string]ControllerSetupFunc
+	versions            map[string]string
+	targetKubeconfig    string
+	namespace           string
+	initialCA           []byte
+	controllers         []string
+	controllerFuncs     map[string]ControllerSetupFunc
+	namespacedInformers map[string]informers.SharedInformerFactory
 }
 
 func (c *ControlPlaneOperatorConfig) Scheme() *runtime.Scheme {
@@ -108,6 +117,50 @@ func (c *ControlPlaneOperatorConfig) TargetKubeClient() kubeclient.Interface {
 		}
 	}
 	return c.targetKubeClient
+}
+
+func (c *ControlPlaneOperatorConfig) TargetConfigClient() configclient.Interface {
+	client, err := configclient.NewForConfig(c.TargetConfig())
+	if err != nil {
+		c.Fatal(err, "cannot get target config client")
+	}
+	return client
+}
+
+func (c *ControlPlaneOperatorConfig) TargetConfigInformers() configinformers.SharedInformerFactory {
+	informerFactory := configinformers.NewSharedInformerFactory(c.TargetConfigClient(), common.DefaultResync)
+	c.Manager().Add(manager.RunnableFunc(func(stopCh <-chan struct{}) error {
+		informerFactory.Start(stopCh)
+		return nil
+	}))
+	return informerFactory
+}
+
+func (c *ControlPlaneOperatorConfig) TargetKubeInformersForNamespace(namespace string) informers.SharedInformerFactory {
+	informer, exists := c.namespacedInformers[namespace]
+	if !exists {
+		informer = informers.NewSharedInformerFactoryWithOptions(c.TargetKubeClient(), common.DefaultResync, informers.WithNamespace(namespace))
+		if c.namespacedInformers == nil {
+			c.namespacedInformers = map[string]informers.SharedInformerFactory{}
+		}
+		c.namespacedInformers[namespace] = informer
+		c.Manager().Add(manager.RunnableFunc(func(stopCh <-chan struct{}) error {
+			informer.Start(stopCh)
+			return nil
+		}))
+	}
+	return informer
+}
+
+func (c *ControlPlaneOperatorConfig) KubeClient() kubeclient.Interface {
+	if c.kubeClient == nil {
+		var err error
+		c.kubeClient, err = kubeclient.NewForConfig(c.Config())
+		if err != nil {
+			c.Fatal(err, "cannot get management kube client")
+		}
+	}
+	return c.kubeClient
 }
 
 func (c *ControlPlaneOperatorConfig) Versions() map[string]string {
